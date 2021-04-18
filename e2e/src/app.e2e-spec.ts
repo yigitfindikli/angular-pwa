d1b940fcec8569111c339097a26206ef112af01b
yigitfindikli <yigitfindikli@gmail.com>
2021-04-18 21:16:40 +0300

    project name updated

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('angular-pwa app is running!');
+    expect(await page.getTitleText()).toEqual('primeng-pwa-quickstart-cli app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-pwa'`, () => {
+  it(`should have as title 'primeng-pwa-quickstart-cli'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-pwa');
+    expect(app.title).toEqual('primeng-pwa-quickstart-cli');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-pwa app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('primeng-pwa-quickstart-cli app is running!');
   });
 });
